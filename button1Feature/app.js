f7ee27032a19aca830febbffc989ba32c45f24a1
Mikel <mikelbrierly@gmail.com>
2016-01-02 11:36:33 -0700

    Button 2 completed on branch

--- button1Feature/app.js
+++ button1Feature/app.js
@@ -2,4 +2,8 @@ $(document).ready(function(){
   $(".clickMe").click(function(){
     alert("This button works!")
   })
+
+  $(".clickMeToo").click(function(){
+    alert("This button also works!")
+  })
 })
